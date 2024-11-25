49c030c2253c4ada9b151ce6da6852c66714ef56
coderAndrew37 <omolloandrew37@gmail.com>
2024-11-25 22:09:34 +0300

    creating the data files

---
+++ public/scripts/data/specialOffersData.js
@@ -0,0 +1,14 @@
+export const specialOffers = [
+  {
+    image: "/images/products/leather-shoes-deal.jpeg",
+    text: "20% Off Premium Leather Shoes - Limited Time!",
+  },
+  {
+    image: "/images/products/jackets-sale.jpeg",
+    text: "Up to 30% Off on Winter Jackets",
+  },
+  {
+    image: "/images/products/free-delivery.jpeg",
+    text: "Free Delivery on Orders Over KSH 10,000",
+  },
+];
